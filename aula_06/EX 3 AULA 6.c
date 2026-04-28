20985d6b7dadb875cad16a16bf71749fc6842258
Yasmin <yasmin.ashaur@aluno.senai.br>
2026-04-28 12:10:52 -0300

    alteração

---
+++ aula_06/EX 3 AULA 6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+
+	int matriz [3][3];
+	int soma = 0;
+
+
+	for (int l=0; l<3; l++) {
+	for(int c=0; c<3; c++) {
+			printf("Digite o valor linha: %i, Coluna %i:", l+1,c+1);
+			scanf("%i", &matriz[l][c]);
+		}
+	}
+	printf("Diagonal principal : \n");
+	for(int l=0; l<3; l++) {
+	for(int c=0; c<3; c++) {
+			if(l == c) {
+				printf("| %i |", matriz[l][c]);
+			}
+		}
+		printf("\n");
+	}
+
+}
